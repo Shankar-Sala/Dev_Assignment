c0496524af8301afee815154148893102a8eeeb2
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-02 15:30:38 +0530

    JavaScript Assignment

---
+++ Javascript/JS_P-1_P-2/app.js
@@ -0,0 +1,28 @@
+ // console.log("hello world");
+// let a = 5;
+// let b = 10;
+// console.log("sum is : ", a+b);
+
+// let a = 10;
+// let b = 8;
+
+// console.log("The total price is : ", a+b , "Rupees.");
+// ? Template literals 
+
+// let output = `The total price is : ${a+b} Rupees.`;
+// console.log(output);
+
+// PQ 
+
+let color = "yellow";
+if(color === "red"){
+    console.log("Stop");
+}
+
+if(color === "yellow"){
+    console.log("Slow Down");
+}
+
+if(color === "Green"){
+    console.log("Go");
+}
