906f0c2902f03923786327459dee64e4d1146e3b
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-02 15:50:16 +0530

    Qs_3.

--- Javascript/JS_P-2_Ass/app.js
+++ Javascript/JS_P-2_Ass/app.js
@@ -1,14 +1,30 @@
-//? Qs1. 
-let num = 190;
+// //? Qs1. 
+// let num = 190;
 
-if(num%10 == 0){
-    console.log("good");
-}else{
-    console.log("Bad");
+// if(num%10 == 0){
+//     console.log("good");
+// }else{
+//     console.log("Bad");
+// }
+
+// //?Qs2. 
+
+// let username = prompt("Enter Your UserName :");
+// let age = prompt("Enter Your Age :");
+// let userAlert = alert(`${username} is ${age} years old`);
+
+//? Qs3.
+let Quarter = 7;
+
+switch (Quarter) {
+    case 1: console.log("January, February, March");
+        break;
+        case 2: console.log("April, May, June");
+        break;
+        case 3: console.log("July, Auguest, September");
+        break;
+        case 4: console.log("October, November, December ");
+        break;
+
+        default : console.log("Please Enter Proper Quarter Number");
 }
-
-//?Qs2. 
-
-let username = prompt("Enter Your UserName :");
-let age = prompt("Enter Your Age :");
-let userAlert = alert(`${username} is ${age} years old`);
