0527f1b51dbfdd28710691801f5a8c120e633606
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-02 16:05:45 +0530

    Qs_4.

--- Javascript/JS_P-2_Ass/app.js
+++ Javascript/JS_P-2_Ass/app.js
@@ -1,4 +1,4 @@
-// //? Qs1. 
+// //? Qs1.
 // let num = 190;
 
 // if(num%10 == 0){
@@ -6,25 +6,36 @@
 // }else{
 //     console.log("Bad");
 // }
-
-// //?Qs2. 
+//! ================================================================
+// //?Qs2.
 
 // let username = prompt("Enter Your UserName :");
 // let age = prompt("Enter Your Age :");
 // let userAlert = alert(`${username} is ${age} years old`);
+//! ================================================================
+// //? Qs3.
+// let Quarter = 7;
 
-//? Qs3.
-let Quarter = 7;
+// switch (Quarter) {
+//     case 1: console.log("January, February, March");
+//         break;
+//         case 2: console.log("April, May, June");
+//         break;
+//         case 3: console.log("July, Auguest, September");
+//         break;
+//         case 4: console.log("October, November, December ");
+//         break;
 
-switch (Quarter) {
-    case 1: console.log("January, February, March");
-        break;
-        case 2: console.log("April, May, June");
-        break;
-        case 3: console.log("July, Auguest, September");
-        break;
-        case 4: console.log("October, November, December ");
-        break;
+//         default : console.log("Please Enter Proper Quarter Number");
+// }
 
-        default : console.log("Please Enter Proper Quarter Number");
+//! ================================================================
+
+//? Qs_4. 
+let fruits = "algorithm";  
+
+if ((fruits[0] == "A" || fruits[0] == "a") && (fruits.length > 5)) {
+    console.log("It is a Golden String");
+} else {
+    console.log("It is not a Golden String");
 }
