84203b59d50de65ed9a64b5953182f743b5d7c64
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-02 15:42:28 +0530

    Js_P-2 Ass_Qs-1,2

---
+++ Javascript/JS_P-2_Ass/app.js
@@ -0,0 +1,14 @@
+//? Qs1. 
+let num = 190;
+
+if(num%10 == 0){
+    console.log("good");
+}else{
+    console.log("Bad");
+}
+
+//?Qs2. 
+
+let username = prompt("Enter Your UserName :");
+let age = prompt("Enter Your Age :");
+let userAlert = alert(`${username} is ${age} years old`);
