a74567461c70d6e81c68f38a0ef2f725fc5fdd38
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-02 17:27:54 +0530

    String Method with arguments

---
+++ Javascript/JS_P-3/app.js
@@ -0,0 +1,32 @@
+// let msg = "      hello      ";
+
+// let password = prompt("set your password");
+// console.log(password.trim());
+
+//?String Method with arguments 
+// stringName.method(arg)   syntax
+
+//! 1) indexOf()
+// let str = "ILoveCoding";
+// let index = str.indexOf("Love");
+
+//! 2) Method chaining
+// let str = "   ILoveCoding     ";
+// let meChain = str.toUpperCase().trim();
+
+
+//! 3) Slice method
+// let str = "ILoveCoding";
+// let Slice = str.slice(1,5);  // last index ignored it shows only (start, end-1)
+
+// console.log(str.slice(-1)); //11-1= 10  it shows you pass index 10 in str.slice()
+
+// //! 3) Replace method
+// let str = "ILoveCoding";
+
+// let Replace = str.replace("Love","Do");
+
+//! 3) Repeat method
+let str = "ILoveCoding";
+
+let Repeat = str.repeat(3);
