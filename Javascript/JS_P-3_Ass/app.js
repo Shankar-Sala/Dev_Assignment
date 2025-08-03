8a3ccc6744e311ca2c3bf1056431e9c2fee12ee2
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-03 12:59:52 +0530

    Qs_2.

--- Javascript/JS_P-3_Ass/app.js
+++ Javascript/JS_P-3_Ass/app.js
@@ -1,7 +1,13 @@
-//?  Qs_1 
-let n = 2;
+//?  Qs_1 To get first n element
+// let n = 2;
+// let num = [7, 9, 0, -2];
+
+// let ans = num.slice(0,n);
+// console.log(ans);
+
+// ? Qs_2 to get the last n elements
+let n = 3;
 let num = [7, 9, 0, -2];
-
-let ans = num.slice(0,n);
-console.log(ans);
-
+let newNum = num.reverse().slice(0,n).reverse();  //  method-1 //shankar
+// let newNum = num.slice(num.length-n);
+console.log(newNum);
