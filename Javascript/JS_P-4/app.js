931893b83fcb8fb4b806e7345ae9a16709a417ab
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-06 20:39:28 +0530

    print odd numbers

--- Javascript/JS_P-4/app.js
+++ Javascript/JS_P-4/app.js
@@ -2,8 +2,15 @@
 // for(let i=1; i<=10; i++){
 //     console.log(i);
 // }
-
+//!==========================================
 // backward loop 
-for(let i=10; i>=1; i--){
+// for(let i=10; i>=1; i--){
+//     console.log(i);
+// }
+
+//!=================================
+// ? Print all odd numbers(1 to 15)
+
+for(let i = 1; i<=15; i += 2){
     console.log(i);
 }
