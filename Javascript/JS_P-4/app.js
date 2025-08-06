b7890b7b5b04f121f40173db3d2887e4ab551abb
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-06 20:56:58 +0530

    print even number

--- Javascript/JS_P-4/app.js
+++ Javascript/JS_P-4/app.js
@@ -10,7 +10,13 @@
 
 //!=================================
 // ? Print all odd numbers(1 to 15)
+// for(let i = 1; i<=15; i += 2){
+//     console.log(i);
+// }
 
-for(let i = 1; i<=15; i += 2){
+// !===============================
+// ? Print all even numbers(2 to 10)
+for(let i = 2 ;i<=10 ;i += 2 ){
     console.log(i);
+  
 }
