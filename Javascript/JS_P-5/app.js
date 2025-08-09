bd950499809f11765fb2ca2d74d2b7ff662f1120
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-09 13:33:39 +0530

    Creating post

---
+++ Javascript/JS_P-5/app.js
@@ -0,0 +1,20 @@
+const student = {
+    name: "shankar",
+    age:23,
+    marks:94.4
+};
+
+
+const item = {
+    price : 100.99,
+    discount : 50,
+    colors : ["red", "pink"] // we can store array in object
+};
+
+const post = {
+    username: "@shankarwebdev_03",
+    content:"This is my #firstPost",
+    likes:150,
+    reposts:5,
+    tags:["@apnacollege", "@delta"]
+}
