1b82e089244821fef2f29f204e43464972c3eac2
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-09 18:28:04 +0530

    Qs. Random Number

--- Javascript/JS_P-5/app.js
+++ Javascript/JS_P-5/app.js
@@ -80,3 +80,10 @@ const student = {
 //     }
 // ];
 
+//? Qs-1 Generate a random number between 1 and 100
+
+// Math.floor(Math.random() * 100)+1;
+// console.log(range);
+
+//? Qs-2 Generate a random number between 1 and 5
+// Math.floor(Math.random() * 5)+1;
