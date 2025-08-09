2e3199dca384a46e06ba7b9ef1811aa3e129e121
Shankar-Sala <shankarsala9995@gmail.com>
2025-08-09 09:15:22 +0530

    Print the multiplication table for 5

--- Javascript/JS_P-4/app.js
+++ Javascript/JS_P-4/app.js
@@ -16,7 +16,27 @@
 
 // !===============================
 // ? Print all even numbers(2 to 10)
-for(let i = 2 ;i<=10 ;i += 2 ){
+// for(let i = 2 ;i<=10 ;i += 2 ){
+//     console.log(i);
+
+// }
+
+// ! ==============================
+// ? Infinite loop
+// for(let i=1; ; i++){
+//     console.log(i);
+// }
+
+// ! ==============================
+// ? Print the multiplication table for 5.
+
+let n = prompt("write your number"); //string
+n = parseInt(n); // convert a string into an integer.
+
+for(let i=n; i<=n*10; i=i+n){
     console.log(i);
-  
 }
+
+// for (let i = 5; i <= 50; i= i+5) {
+//        console.log(i);
+// }
